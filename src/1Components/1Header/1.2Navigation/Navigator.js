9202369e408ecbdec166beedaf438cc59864f486
Barbos <artur.korol.2000.10.25@mail.ru>
2022-08-09 15:55:47 +0300

    fix nav

--- src/1Components/1Header/1.2Navigation/Navigator.js
+++ src/1Components/1Header/1.2Navigation/Navigator.js
@@ -1,25 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import style from "./Navigator.module.scss";
 
 export const Navigator = () => {
-    const [active, setActive] = useState(false)
-    return (
-        <div className={style.navContainer}>
-            <nav className={style.nav}
-                onClick={() => {
-                    setActive(!active);
-                }}>
-                <NavLink className={active ? style.active : ''} to="/woman">
-                    WOMAN
-                </NavLink>
-                <NavLink className={''} to="">
-                    MAN
-                </NavLink>
-                <NavLink className={''} to="">
-                    KIDS
-                </NavLink>
-            </nav>
-        </div>
-    );
-}
+  return (
+    <div className={style.navContainer}>
+      <nav className={style.nav}>
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    color: "#5ece7b",
+                    borderBottom: "2px solid #5ece7b",
+                  }
+                : null
+            }
+            to="/woman"
+          >
+            WOMAN
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    color: "#5ece7b",
+                    borderBottom: "2px solid #5ece7b",
+                  }
+                : null
+            }
+            to="/man"
+          >
+            MAN
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    color: "#5ece7b",
+                    borderBottom: "2px solid #5ece7b",
+                  }
+                : null
+            }
+            to="/kids"
+          >
+            KIDS
+          </NavLink>
+        </li>
+      </nav>
+    </div>
+  );
+};
